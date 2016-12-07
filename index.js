b65c967ca23f43149f4f8f3e999b13e809040c8e
Kornel Lesiński <kornel@geekhood.net>
2016-12-07 01:42:15 +0000

    Show hostname

--- index.js
+++ index.js
@@ -170,4 +170,7 @@ app.use(express.static('public', {
     redirect: false,
 }));
 
-app.listen(process.env.PORT || 3333);
+const port = process.env.PORT || 3333;
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}/`);
+});
